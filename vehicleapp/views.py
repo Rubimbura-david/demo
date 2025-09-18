ae1812ab3322c8d5bf8626ea42fe0f576fe0c9db
kofi <rubdavid20@gmail.com>
2025-09-18 15:42:39 +0200

    first commit

---
+++ vehicleapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def clients(request):
+    return render(request, 'clients.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):   
+    return render(request, 'services.html')
+
+def contact(request):    
+    return render(request, 'contact.html')
